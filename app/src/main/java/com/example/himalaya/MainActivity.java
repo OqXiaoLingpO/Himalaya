21cfe32ede2daca9f241f26a164eb3f5f08bb9c4
OqXiaoLingpO <475140353@qq.com>
2020-02-03 20:03:35 +0800

    第一次提交

--- app/src/main/java/com/example/himalaya/MainActivity.java
+++ app/src/main/java/com/example/himalaya/MainActivity.java
@@ -1,7 +1,18 @@
 package com.example.himalaya;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.category.Category;
+import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Map<String, String> map = new HashMap<String, String>();
+        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
+            @Override
+            public void onSuccess(@Nullable CategoryList categoryList) {
+                List<Category> categories = categoryList.getCategories();
+                if (categories!=null) {
+                    int size = categories.size();
+                    Log.d("Tag","categories ------->"+size);
+                    for (Category category : categories) {
+                        Log.d("MainActivity","categories---=-->"+category.getCategoryName());
+                    }
+                }
+            }
+            @Override
+            public void onError(int i, String s) {
+                Log.d("dsfsf","error code----->"+i+"error msg----->"+s);
+            }
+        });
     }
 }
